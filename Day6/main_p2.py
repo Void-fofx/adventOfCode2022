f0b45dc93a80d502ae97b528c2095fb292a754dc
void-f(x) <87593145+Void-fofx@users.noreply.github.com>
2022-12-16 19:10:48 -0500

    Part 2 commit

---
+++ Day6/main_p2.py
@@ -0,0 +1,26 @@
+def dup(li: str):
+    li = sorted(li)
+    for i in range(len(li)-1):
+        if li[i] == li[i+1]:
+            return True
+    return False
+
+
+def scan(file, pattern=14):
+    numbers = []
+    with open(file) as f:
+        text = f.readlines()
+        count = 1
+        for i in range(len(text[0]) - pattern):
+            if dup(text[0][i:i+pattern]):
+                count += 1
+            else:
+                print(f'{len(text[0][:count+pattern-1])}')
+                numbers.append(count + pattern-1)
+                count = 0
+                break
+    print(numbers)
+
+
+if __name__ == '__main__':
+    scan('input.txt')
